7ce491f10d4784daad9d1bcbdd14044d72e2c6ee
luaselles <lu.selles@unoeste.edu.br>
2022-10-12 12:45:21 -0300

    CRUD Scaffolding

---
+++ ProjectAplicationWebMVC/Data/ProjectAplicationWebMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjectAplicationWebMVC.Models;
+
+namespace ProjectAplicationWebMVC.Data
+{
+    public class ProjectAplicationWebMVCContext : DbContext
+    {
+        public ProjectAplicationWebMVCContext (DbContextOptions<ProjectAplicationWebMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjectAplicationWebMVC.Models.Department> Department { get; set; }
+    }
+}
--- ProjectAplicationWebMVC/Startup.cs
+++ ProjectAplicationWebMVC/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjectAplicationWebMVC.Data;
 
 namespace ProjectAplicationWebMVC
 {
@@ -33,6 +35,9 @@ namespace ProjectAplicationWebMVC
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<ProjectAplicationWebMVCContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ProjectAplicationWebMVCContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
